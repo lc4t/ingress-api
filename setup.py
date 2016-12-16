3caf09675f488e27c13b629104ccc6e2c7b6ead8
lc4t <lc4t0.0@gmail.com>
2016-12-17 03:57:52 +0800

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+
 setup(
-    name='ingressAPI',
-    packages=['requests[socks]', 'lxml', 'beautifulsoup4'],
-    version='0.1',
-    description='Game Ingress API',
-    author='lc4t',
-    author_email='lc4t0.0@gmail.com',
-    url='https://github.com/lc4t/ingress-api',  # use the URL to the github repo
-    download_url='https://github.com/lc4t/ingress-api/tarball/0.1',  # I'll explain this in a second
-    keywords=['ingress', 'api', 'google', 'intel', 'map'],  # arbitrary keywords
-    classifiers=[],
+    name = 'ingressAPI',
+    packages = ['ingressAPI'],
+    version = '0.1',
+    description = 'Game Ingress API',
+    author = 'lc4t',
+    author_email = 'lc4t0.0@gmail.com',
+    keywords = ['ingress', 'api', 'google', 'intel', 'map'],
+    install_requires = ['requests[socks]', 'lxml', 'beautifulsoup4'],
+    license = 'GPL3.0',
+    url = 'https://github.com/lc4t/ingress-api',
+    download_url = 'https://github.com/lc4t/ingress-api/tarball/0.1',
 )
